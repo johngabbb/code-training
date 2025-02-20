50309a99ba39f75636e55f7314e164e8e64c1b79
John Gabriel <johngaelreyes@gmail.com>
2025-02-20 22:00:43 +0800

    Progress 2 - Add Portfolio inc

--- FinanceProject/frontend/src/Components/CardList/CardList.tsx
+++ FinanceProject/frontend/src/Components/CardList/CardList.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Card from "../Card/Card";
+import { CompanySearch } from "../../company";
+import { v4 as uuidv4 } from "uuid";
 
-interface Props {}
+interface Props {
+  searchResults: CompanySearch[];
+}
 
-const CardList = (props: Props) => {
+const CardList = ({ searchResults }: Props) => {
   return (
-    <div>
-      <Card companyName="Max" ticker="Beagle" price={99} />
-      <Card companyName="Hunter" ticker="Husky" price={99} />
-    </div>
+    <>
+      {searchResults.length > 0 ? (
+        searchResults.map((result) => {
+          return (
+            <Card id={result.symbol} key={uuidv4()} searchResult={result} />
+          );
+        })
+      ) : (
+        <h1>No results.</h1>
+      )}
+    </>
   );
 };
 
