17d64390dbe82332ab155acf5fc1fc0f4e5bdbc7
John Gabriel <johngaelreyes@gmail.com>
2025-03-19 23:44:24 +0800

    Semi update variableData

--- Certification-Exam/FreeCodeChamp/CsharpProjects/VariableData/Program.cs
+++ Certification-Exam/FreeCodeChamp/CsharpProjects/VariableData/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region First Chapter
+//Console.WriteLine("Signed integral types:");
+
+//Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+//Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+//Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+//Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+
+
+//Console.WriteLine("");
+//Console.WriteLine("Unsigned integral types:");
+
+//Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+//Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+//Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+//Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+
+//Console.WriteLine("");
+//Console.WriteLine("Floating point types:");
+//Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+//Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+//Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+#endregion
+
+decimal myDecimal = 3.14m;
+Console.WriteLine($"decimal: {myDecimal}");
+
+int myInt = (int)myDecimal;
+Console.WriteLine($"int: {myInt}");
+
+float myFloat = (float)myInt;
+Console.WriteLine($"float: {myInt}");
+
+
