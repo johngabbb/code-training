4cdf58df923f53366e540cbe58241fa3dbcce6c0
John Gabriel <johngaelreyes@gmail.com>
2025-03-20 21:40:45 +0800

    unit 4/10

--- Certification-Exam/FreeCodeChamp/CsharpProjects/VariableData/Program.cs
+++ Certification-Exam/FreeCodeChamp/CsharpProjects/VariableData/Program.cs
@@ -102,5 +102,26 @@ foreach (var pallet in pallets)
 {
     Console.WriteLine($"-- {pallet}");
 }
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 3);
+Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
 #endregion
 
