5c9d7666618db54973d5c14a6ad3e237447aab09
John Gabriel <johngaelreyes@gmail.com>
2025-03-20 21:37:10 +0800

    DataVariable end chapter4

--- Certification-Exam/FreeCodeChamp/CsharpProjects/VariableData/Program.cs
+++ Certification-Exam/FreeCodeChamp/CsharpProjects/VariableData/Program.cs
@@ -22,13 +22,85 @@
 //Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
 #endregion
 
-decimal myDecimal = 3.14m;
-Console.WriteLine($"decimal: {myDecimal}");
+#region Second Chapter
+//decimal myDecimal = 3.14m;
+//Console.WriteLine($"decimal: {myDecimal}");
 
-int myInt = (int)myDecimal;
-Console.WriteLine($"int: {myInt}");
+//int myInt = (int)myDecimal;
+//Console.WriteLine($"int: {myInt}");
 
-float myFloat = (float)myInt;
-Console.WriteLine($"float: {myInt}");
+//float myFloat = (float)myInt;
+//Console.WriteLine($"float: {myInt}");
+#endregion
 
+#region Third Chapter
+//string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+//decimal total = 0;
+//string message = string.Empty;
+
+//foreach (var value in values )
+//{
+//    bool numericValue = decimal.TryParse(value, out decimal result);
+//    if (numericValue)
+//    {
+//        total += result;
+//    } 
+//    else
+//    {
+//        message = message + value;
+//    }
+//}
+
+//Console.WriteLine($"Total: {total}");
+//Console.WriteLine($"Message: {message}");
+
+//int value1 = 11;
+//decimal value2 = 6.2m;
+//float value3 = 4.3f;
+
+//// Your code here to set result1
+//// Hint: You need to round the result to nearest integer (don't just truncate)
+
+//int result1 = Convert.ToInt32(value1 / value2);
+//decimal result2 = value2 / (decimal)value3;
+//float result3 = value3 / value1;
+
+
+//Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+//// Your code here to set result2
+//Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+//// Your code here to set result3
+//Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+#endregion
+
+#region Fourth Chapter
+//string[] pallets = ["B14", "A11", "B12", "A13"];
+
+//Console.WriteLine("Sorted...");
+//Array.Sort(pallets);
+//foreach (var pallet in pallets)
+//{
+//    Console.WriteLine($"-- {pallet}");
+//}
+
+//Console.WriteLine("");
+//Console.WriteLine("Reversed...");
+//Array.Reverse(pallets);
+//foreach (var pallet in pallets)
+//{
+//    Console.WriteLine($"-- {pallet}");
+//}
+
+string[] pallets = ["B14", "A11", "B12", "A13"];
+Console.WriteLine("");
+
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+#endregion
 
