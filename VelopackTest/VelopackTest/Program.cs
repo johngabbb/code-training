30bc70a8b60c81354fe1ef65c09c4ec09d51a330
John Gabriel <johngaelreyes@gmail.com>
2025-07-27 18:31:41 +0800

    Add existing file updates yrs before

---
+++ VelopackTest/VelopackTest/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace VelopackTest;
+
+public class Program
+{
+    static void Main()
+    {
+
+        TestWriteLine("VELOPACK");
+    }
+
+    static void TestWriteLine(string text)
+    {
+        Console.WriteLine(text);
+    }
+}
